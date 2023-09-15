9bd599ed20421e754f4dee579c4c4def256c834a
gabriel-tobing <gabrieltobing789@gmail.com>
2023-09-15 10:44:27 +0700

    Membuat tampilan Sidebar

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -11,6 +11,7 @@ const HomePage = () => {
         <NavbarLayout />
         <MainLayout>
           <ToDoListLayout />
+          <div>Ini Kalendar</div>
         </MainLayout>
       </div>
     </div>
